c07953431c2b271675f7cd0379edf7ac81b0dee9
Andrew Nero <andrew.nero@gmail.com>
2018-11-08 20:50:44 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+// app.get('/', (req, res) => {
+//
+// });
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
